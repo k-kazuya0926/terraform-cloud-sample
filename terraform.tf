044d6055f00cd7188f309a087bdc47c7568953bd
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-03-27 09:23:49 +0900

    初期設定

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "1.4.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.59.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "kazuya-kobayashi"
+
+    workspaces {
+      name = "example"
+    }
+  }
+}
